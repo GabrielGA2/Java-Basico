4d3bd5a9fcffd255902144ab16eac95884af826e
GabrielGA2 <g.guedes2@hotmail.com>
2022-10-21 10:51:43 -0300

    Primeiro projeto

---
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -0,0 +1,18 @@
+public class MinhaClasse {
+ 
+ public static void main (String [] args) {
+
+  String primeiroNome = "Gabriel";
+  String segundoNome="Guedes"; 
+
+  String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+    
+  System.out.println(nomeCompleto);
+} 
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+ return "Resultado do Método " + primeiroNome.concat(" ").concat (segundoNome);  
+}
+
+
+}
